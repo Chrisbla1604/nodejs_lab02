d3e76a551c54948d100a8ab275f2444b32443ad2
Chrisbla1604 <chrisbla160487@gmail.com>
2024-06-06 11:19:30 -0500

    Middelware Sesion bodyparser and POST from postman

--- index.js
+++ index.js
@@ -5,17 +5,56 @@ const express = require('express');
 const engine = require('ejs-locals');
 const port =3005;
 const app = express();
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+// Use the session middleware
+
+app.use(session({
+    secret: 'Bootcamp2024ESPOL',
+    cookie: { maxAge: 60000 }
+})); 
+    
 
 app.use('/',express.static(__dirname+'/public'));
 app.engine('ejs', engine);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.use((request,response,next) => {
+    console.log('Entra al middleware 1');
+
+    console.log('Body indefined:',request.body);
+
+    request.user ={
+        curso:'Bootcamp',
+        pais: 'Ecuador'
+    };
+    
+    next();
+});
+
+app.use(bodyParser.json({ type: 'application/json' }))
+
 
 app.get('/api',(request,response)=>{
 
+console.log('Metodo GET path /api') ;  
+console.log('Pais:',request.user);
 response.render('ejs/servercarshop/index');
 });
+
+
+app.post('/api',(request,response)=>{
+
+console.log('Metodo POST path /api') ;
+
+request.body.product.type= 'vegetable';
+request.body.parser=true;
+console.log(request.body) ;
+response.json(request.body);
+});
+
 
 
 
