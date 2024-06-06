c413b05cf7c6a9bab0fa0dc9a30787e35478d9cb
Chrisbla1604 <chrisbla160487@gmail.com>
2024-06-06 06:29:13 -0500

    Mongo Data Base Schema and function Create - Find - Delete

--- db.js
+++ db.js
@@ -7,3 +7,68 @@ mongoose.connect(URL)
 .then(() => {
 console.log('Mongo Database connected!');
 })
+.catch((error) => {
+    console.log('Error in Data Base:', error);
+})
+
+
+const user_registers = mongoose.Schema({
+
+    nombre: String,
+    correo: String,
+    telefono: String,
+    identificacion: String,
+    marca: String,
+    modelo: String,
+    placa: String,
+    nivelgasolina: String,
+    observaciones: String,
+    cambiofrenos: Boolean,
+    cambioaceite: Boolean,
+    diagnosticogeneral: Boolean,
+    alineacionbalanceo: Boolean,
+    sistemaelectrico: Boolean
+
+    });
+    const User_Registers = new mongoose.model('user_register',user_registers);
+
+const new_user_register = new User_Registers({
+    nombre: "Estefano",
+    correo: "nodejs@bootcamp.com",
+    telefono: "0923456788",
+    identificacion: "0931789347",
+    marca: "Nissan",
+    modelo: "Duster",
+    placa: "GHS-2354",
+    nivelgasolina: "lleno",
+    observaciones: "sin novedades",
+    cambiofrenos: false,
+    cambioaceite: false,
+    diagnosticogeneral: false,
+    alineacionbalanceo: true,
+    sistemaelectrico: true
+})
+.save()
+.then((dbregister)=> {
+      console.log('New register:', dbregister);
+})
+.catch((error)=>{
+     console.log('Error:',error);
+})
+
+
+User_Registers.deleteMany({
+    nombre:'Christian'
+} ).then((dataerased)=>{
+    console.log('Object Erased:',dataerased);
+})
+
+
+User_Registers.find({
+    nombre:'Christian', 
+    identificacion:'0931789347' 
+} ).then((datafound)=>{
+    console.log('Object Found:',datafound);
+}).catch((error)=>{
+    console.log('Error:',error);
+})
